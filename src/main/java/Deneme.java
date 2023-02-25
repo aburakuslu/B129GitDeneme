98d4057bdfa230745057b846c476b545e6a41b77
aburakuslu <alpburakuslu@gmail.com>
2023-02-25 19:29:11 +0300

    v4

--- src/main/java/Deneme.java
+++ src/main/java/Deneme.java
@@ -2,6 +2,8 @@ public class Deneme {
 
     public static void main(String[] args) {
         System.out.println("Merhaba!");
+        System.out.println("Dunya");
+        System.out.println("B129");
     }
 }
 //Restore --> Working Space'i tamamen temizler.
